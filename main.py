4a7c25f3e6e2de44e776a36289c7be55b3cdaae8
Small_Giant <legohaly@gmail.com>
2025-11-28 22:47:04 +0100

    remove hardcode paths

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sys
 from stats import count_book_words, count_book_characters, sort_list
+
+print("Usage: python3  main.py /home/aronm/workspace/github.com/Small-Giant-Code/BookBot/books/frankenstein.txt")
+print(sys.argv)
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-filepath = "/home/aronm/workspace/github.com/Small-Giant-Code/BookBot/books/frankenstein.txt"
+filepath = sys.argv[0]
 book_text = get_book_text(filepath)
 #print(book_text)
 
@@ -22,6 +27,7 @@ for i in range(0, len(list_of_dictionaries)):
     value = dict["num"]
     print(f"{key}: {value}")
 
+print(sys.argv)
 
 
 
